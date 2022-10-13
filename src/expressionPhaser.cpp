e48828f1d4f2b81dd0faa64ddc632b14459cc155
IceLocke <lockeice@gmail.com>
2022-10-14 01:05:54 +0800

    update caculate_expression()

--- src/expressionPhaser.cpp
+++ src/expressionPhaser.cpp
@@ -1,3 +1,5 @@
+// Code Reference: https://blog.csdn.net/qq_45768060/article/details/105414612
+
 #include "expressionPhaser.h"
 #include "function.cpp"
 #include "expression.h"
@@ -6,33 +8,28 @@
 #include <vector>
 #include <string>
 #include <cstring>
+#include <functional>
 
 using std::isdigit;
+using std::function;
 using std::string;
 using std::vector;
 using std::stack;
 using std::map;
-
-enum stringType {
-    NUMBER = 0, 
-    VARIABLE = 1, 
-    FUNCTION = 2, 
-    OPERATOR = 3
-};
 
 stack<string> operators;
 stack<number> nums;
 vector<string> strings;
 vector<stringType> string_types;
 map<string, expression> &var;
-map<string, bool> &functions;
+map<string, function<number(number)>> &functions;
 
 // Divide string to parts: numbers, variables, functions, and operators
 // Consider situations that may occur in the input
 // 1 - Numbers: directly convert to 'number' class
 // 2 - Variables: use the map to convert to number
 // 3 - Operators: +, -, *, /, and ()
-// 4 - Math functions: consider as operators, together with (
+// 4 - Math functions
 // assume that input is valid
 void divide_string(expression e) {
     string &str = e.str;
@@ -87,8 +84,34 @@ bool is_equation(expression e) {
     return false;
 }
 
-void caculate() {
+bool is_function(string s) {
+    if (s == "+" || s == "-" || s == "*" || s == "/" || s == "%" || s == "^" || s == "(" || s == ")")
+        return true;
+    else return false;
+}
 
+void caculate(bool is_function) {
+    if (is_function) {
+        function<number(number)> &func = functions[operators.top()];
+        number top_num = nums.top();
+        nums.pop();
+        nums.push(func(top_num));
+        operators.pop();
+        return;
+    }
+    
+    
+}
+
+// Priority List:
+operatorPriority operator_priority(char ch) {
+    if (ch == '+' || ch == '-')
+        return LOW;
+    if (ch == '*' || ch == '/' || ch == '%')
+        return MEDIUM;
+    if (ch == '^')
+        return HIGH;
+    return NOT_OPERATOR;
 }
 
 expression caculate_expression(expression e, map<string, expression> &variables) {
@@ -97,6 +120,30 @@ expression caculate_expression(expression e, map<string, expression> &variables)
 
     int strings_size = strings.size();
     for (int i = 0; i < strings_size; i++) {
-
+        if (string_types[i] == OPERATOR) {
+            if (operator_priority(strings[i][0]) == LOW) {
+                while (!operators.empty() && operators.top()[0] != '(')
+                    caculate(false);
+                operators.push(strings[i]);
+            }
+            // *, /, %, high priority
+            else if (operator_priority(strings[i][0] == MEDIUM)) {
+                while (!operators.empty() && operator_priority(operators.top()[0]) == MEDIUM)
+                    caculate(false);
+                operators.push(strings[i]);
+            }
+            else if (operator_priority(strings[i][0] == HIGH))
+                operators.push(strings[i]);
+            else if (strings[i][0] == '(')
+                operators.push(strings[i]);
+            else if (strings[i][0] == ')') {
+                while (operators.top() != "(")
+                    caculate(false);
+                operators.pop();
+                if (!operators.empty())
+                    if (is_function(operators.top()))
+                        caculate(true);
+            }
+        }
     }
 }
